40cc8551a334708ba145338124ff5ce6cf00eeac
ralph_the_badger <badger_rr@gmx.de>
2019-08-21 23:58:23 +0200

    Day 3

--- routes/contact.js
+++ routes/contact.js
@@ -3,13 +3,17 @@ const express = require("express");
 
 const router = express.Router();
 
+const person = [];
+
 router.use("/contact", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "views", "contact.html"));
 });
 
 router.post("/new_contact", (req, res, next) => {
-  console.log(req.body.message);
+  person.push({ personName: req.body.newName });
   res.redirect("/");
 });
 
 module.exports = router;
+// module.exports = person;
+// exports.bubu = "Bubu";
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,21 @@
 const path = require("path");
 const express = require("express");
 
+const contactData = require("./contact");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "home.html"));
+  const myContactData = contactData.person;
+  const myBubu = contactData.bubu;
+  console.log(myContactData, myBubu);
+  res.render("home", {
+    pageTitle: "Startseite",
+    PN: [0, 1, 2],
+    perso: myContactData,
+    lala: myBubu
+  });
+  // res.sendFile(path.join(__dirname, "../", "views", "home.html"));
 });
 
 module.exports = router;
