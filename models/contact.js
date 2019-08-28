1bbf6c1e55c3bc297f79e08be403e3811c8b2b17
ralph_the_badger <badger_rr@gmx.de>
2019-08-28 22:09:41 +0200

    Day 10

--- models/contact.js
+++ models/contact.js
@@ -1,42 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 
-const contacts = [];
+// define path to JSON-File
+const contactFilePath = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "contacts.json"
+);
 
+/* 
+read data from JSON-File: 
+if file is empty -> create empty array, otherwise return data in array
+callback (cb) is needed because data must be rendered after page was loaded (async)
+*/
+const getContactsFromFile = cb => {
+  fs.readFile(contactFilePath, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      return cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+// create class for single product with methods save and get all praoducts from file
 module.exports = class Contact {
   constructor(name) {
     this.name = name;
   }
+  // read JSON-file and push data as strings into it, if error occurs -> show error
   save() {
-    const contactFilePath = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "contacts.json"
-    );
-    fs.readFile(contactFilePath, (err, fileContent) => {
-      let contacts = [];
-      if (!err) {
-        contacts = JSON.parse(fileContent);
-        // console.log(fileContent);
-      }
+    getContactsFromFile(contacts => {
       contacts.push(this);
       fs.writeFile(contactFilePath, JSON.stringify(contacts), err => {
         console.log(err);
       });
     });
   }
+  // read JSON-file and get all products from file
   static fetchAll(cb) {
-    const contactFilePath = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "contacts.json"
-    );
-    fs.readFile(contactFilePath, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(fileContent));
-      }
-    });
+    getContactsFromFile(cb);
   }
 };
